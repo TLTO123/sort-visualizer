27f7ca9c43a972c53b8fb9a4279f1596f0c6aa26
TLTO123 <cantareroalex771@gmail.com>
2026-08-18 17:15:20 -0600

    Navbar y offcanvas base para el sitio

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<template>
+  <header>
+
+  </header>
+  <nav class="bg-[#141414]">
+    <div class="flex flex-row border-b-4 border-b-[#9D9D9D] p-3">
+      <div class="basis-1/16 flex justify-center">
+        <button class="hover:cursor-pointer" type="button" id="openMenu">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="yellow"
+            stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu">
+            <line x1="4" x2="20" y1="12" y2="12" />
+            <line x1="4" x2="20" y1="6" y2="6" />
+            <line x1="4" x2="20" y1="18" y2="18" />
+          </svg>
+        </button>
+      </div>
+      <div class="basis-14/16 flex justify-center">
+          <h1 class="text-white lexend text-fluid-xl">Sort Visualizer</h1>
+      </div>
+    </div>
+    <div class="flex flex-row bg-[#193264] p-2 justify-center">
+      <h2 class="text-white lexend text-fluid-base">Inicio</h2>
+    </div>
+  </nav>
+  <main>
+
+  </main>
+
+  <!-- Overlay -->
+  <div id="overlay" class="fixed inset-0 z-40 hidden bg-black/50"></div>
+  
+  <!--Contenido dentro del offcanvas-->
+  <div class="fixed bottom-0 left-0 top-0 z-1045 flex w-96 max-w-full -translate-x-full flex-col border-none bg-[#141414] bg-clip-padding text-white shadow-sm outline-none transition duration-300 ease-in-out data-twe-offcanvas-show:transform-none" tabindex="-1" id="offcanvas" aria-labelledby="offcanvasLabel" data-twe-offcanvas-init>
+    <div class="flex items-center justify-between p-4 mt-5">
+      <h2 class="text-xl lexend font-bold">
+            Menú
+      </h2>
+      <button id="closeMenu" type="button" class="rounded-sm py-1 px-2 text-gray-500 hover:bg-red-500 hover:text-white hover:cursor-pointer">
+            ✕
+      </button>
+    </div>
+    <nav class="p-4 mt-10">
+      <ul class="space-y-2">
+        <li>
+          <a href="/" class="block text-fluid-base lexend rounded-lg px-4 py-3 hover:bg-yellow-500">Inicio</a>
+        </li>
+        <li>
+          <a href="/" class="block text-fluid-base lexend rounded-lg px-4 py-3 hover:bg-yellow-500">Catálogo</a>
+        </li>
+        <li>
+          <a href="/" class="block text-fluid-base lexend rounded-lg px-4 py-3 hover:bg-yellow-500">Rendimiento</a>
+        </li>
+        <li>
+          <a href="/" class="block text-fluid-base lexend rounded-lg px-4 py-3 hover:bg-yellow-500">Ordenar mi lista</a>
+        </li>
+        <li>
+          <a href="/" class="block text-fluid-base lexend rounded-lg px-4 py-3 hover:bg-yellow-500">Desarrolladores</a>
+        </li>
+        <li>
+          <a href="/" class="block text-fluid-base lexend rounded-lg px-4 py-3 hover:bg-yellow-500">Contacto</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,34 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+createApp(App).mount('#app');
+
+const openMenu = document.getElementById("openMenu");
+const closeMenu = document.getElementById("closeMenu");
+const overlay = document.getElementById("overlay");
+
+const offcanvas = document.getElementById("offcanvas");
+
+
+function openOffcanvas() {
+
+    offcanvas.classList.remove("-translate-x-full");
+    offcanvas.classList.add("translate-x-0");
+
+    overlay.classList.remove("hidden");
+
+}
+
+function closeOffcanvas() {
+
+    offcanvas.classList.remove("translate-x-0");
+    offcanvas.classList.add("-translate-x-full");
+
+    overlay.classList.add("hidden");
+
+}
+
+openMenu.addEventListener("click", openOffcanvas);
+closeMenu.addEventListener("click", closeOffcanvas);
+overlay.addEventListener("click", closeOffcanvas);
