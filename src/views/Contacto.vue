629f4a791505090c3a66707bfefca3c42598722f
BerryBGoode <fernandomena3131@gmail.com>
2026-08-29 17:02:51 -0600

    vista contacto

--- src/views/Contacto.vue
+++ src/views/Contacto.vue
@@ -2,8 +2,41 @@
 </script>
 
 <template>
-    <h1>Contacto</h1>
+    <div class="container-main">
+        <div class="intro">
+            <h1 class="intro-title">Comprende los algoritmos de ordenamiento visualmente</h1>
+            <h1 class="m-5">
+                ¿Tienes alguna duda, sugerencia o comentario sobre SortVisualizer? Nos interesa conocer tu
+                opinión y ayudarte en todo lo relacionado con la aplicación. Utiliza el formulario de contacto para
+                enviarnos tus consultas, reportar inconvenientes o compartir ideas que contribuyan a mejorar la
+                experiencia de todos los usuarios.
+            </h1>
+        </div>
+        <div
+            class="
+            container-form-contacto p-5 [--color-base:#1E1E1E] 
+            text-white bg-[linear-gradient(to_bottom,var(--color-base),color-mix(in_srgb,var(--color-base)_95%,white))] 
+            px-[5vw] flex ">
+            <form class="bg-[#272727] rounded border border-black w-full">
+                <div class="form">
+                    <label for="txtNombre">Nombre</label>
+                    <input class="bg-[#484848] border border-black rounded" type="text" name="txtNombre" id="txtNombre">
+
+                    <label for="txtEmail">Correo electrónico</label>
+                    <input class="bg-[#484848] border border-black rounded" type="email" name="txtEmail" id="txtEmail">
+
+                    <label for="txtMensaje">Mensaje</label>
+                    <textarea class="bg-[#484848] border border-black rounded" name="txtMensaje" id="txtMensaje"></textarea>
+
+                </div>
+
+                <button type="submit" class="cursor-pointer bg-[#FFE77A] pt-2 pb-2 pl-3 pr-3 rounded text-black font-bold">Enviar</button>
+            </form>
+        </div>
+
+    </div>
 </template>
 
 <style>
+
 </style>
--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,11 +1,11 @@
 <script setup>
 import { onMounted } from 'vue';
-import {graficaBarra} from '../components/charts'
-import {RouterLink} from 'vue-router'
+import { graficaBarra } from '../components/charts'
+import { RouterLink } from 'vue-router'
 
 
-const etiquetas = ['', '', '', '', '', '', '', '', '', '',''];
-const valores =   [45, 12, 28, 35, 25, 10, 5, 50, 40, 20, 15 ];
+const etiquetas = ['', '', '', '', '', '', '', '', '', '', ''];
+const valores = [45, 12, 28, 35, 25, 10, 5, 50, 40, 20, 15];
 
 onMounted(() => {
     graficaBarra('grafica-barra', 'Métodos de ordenamiento', etiquetas, valores)
@@ -17,122 +17,131 @@ onMounted(() => {
     <div class="container-main">
         <div class="intro">
             <h1 class="intro-title">Comprende los algoritmos de ordenamiento visualmente</h1>
-            <h1 class="m-5">Una plataforma interactiva para ver, entender y comparar el <strong class="font-bold from-pink-400 via-orange-400 to-yellow-400 bg-clip-text text-transparent">comportamiento de las estructuras de datos en tiempo real.</strong></h1>
+            <h1 class="m-5">Una plataforma interactiva para ver, entender y comparar el <strong
+                    class="font-bold from-pink-400 via-orange-400 to-yellow-400 bg-clip-text text-transparent">comportamiento
+                    de las estructuras de datos en tiempo real.</strong></h1>
         </div>
         <div class="text-intro">
             <p class="m-5 font-mono">
-                Bienvenido a SortVisualizer. Esta herramienta fue diseñada para transformar conceptos lógicos 
-                abstractos en animaciones claras, intuitivas y dinámicas. Ya sea que estés dando tus primeros 
-                pasos en la programación o quieras profundizar en la eficiencia de tu código, aquí podrás experimentar 
-                de forma práctica cómo operan los algoritmos esenciales de las ciencias de la computación. Observa el 
-                flujo de los datos paso a paso, ajusta la velocidad de ejecución y descubre exactamente qué ocurre detrás
+                Bienvenido a SortVisualizer. Esta herramienta fue diseñada para transformar conceptos lógicos
+                abstractos en animaciones claras, intuitivas y dinámicas. Ya sea que estés dando tus primeros
+                pasos en la programación o quieras profundizar en la eficiencia de tu código, aquí podrás experimentar
+                de forma práctica cómo operan los algoritmos esenciales de las ciencias de la computación. Observa el
+                flujo de los datos paso a paso, ajusta la velocidad de ejecución y descubre exactamente qué ocurre
+                detrás
                 de cada intercambio.
             </p>
             <a class="btn-rendimiento p-2 mt-2">
                 <router-link to="/rendimiento" class="font-black">Ir a la prueba de rendimiento</router-link>
             </a>
-        
+
         </div>
         <div class="charts">
             <canvas id="grafica-barra" class="mt-3"></canvas>
         </div>
         <div class="cards pt-10 pb-10">
-            <div class="interactivo mb-10">
+            <div class="interactivo mb-10 p-5">
                 <h3 class="font-bold mb-2">Interactivo</h3>
-                <p class="pl-4">Toma el mando de la simulación ajustando la velocidad de las animaciones en tiempo real de lento a rápido 
-                    para no perderte ningún detalle. Experimenta de forma directa generando arreglos aleatorios o introduciendo 
+                <p class="pl-4">Toma el mando de la simulación ajustando la velocidad de las animaciones en tiempo real
+                    de lento a rápido
+                    para no perderte ningún detalle. Experimenta de forma directa generando arreglos aleatorios o
+                    introduciendo
                     tus propias listas personalizadas de datos.
                 </p>
             </div>
-            <div class="educativo mb-10">
+            <div class="educativo mb-10 p-5">
                 <h3 class="font-bold mb-2">Educativo</h3>
                 <p class="pl-4">
-                    Toma el mando de la simulación ajustando la velocidad de las animaciones en tiempo real de lento a 
-                    rápido para no perderte ningún detalle. Experimenta de forma directa generando arreglos aleatorios 
+                    Toma el mando de la simulación ajustando la velocidad de las animaciones en tiempo real de lento a
+                    rápido para no perderte ningún detalle. Experimenta de forma directa generando arreglos aleatorios
                     o introduciendo tus propias listas personalizadas de datos.
                 </p>
             </div>
-            <div class="medicion-real mb-10">
+            <div class="medicion-real mb-10 p-5">
                 <h3 class="font-bold mb-2">Medición Real</h3>
                 <p class="pl-4">
-                    Ve más allá de la teoría de la complejidad algorítmica analizando la eficiencia en un entorno práctico. 
-                    La herramienta cuenta con cronómetros integrados de alta precisión que registran el tiempo de ejecución.
+                    Ve más allá de la teoría de la complejidad algorítmica analizando la eficiencia en un entorno
+                    práctico.
+                    La herramienta cuenta con cronómetros integrados de alta precisión que registran el tiempo de
+                    ejecución.
                 </p>
             </div>
         </div>
-    
+
     </div>
 
 </template>
 
 <style>
-*{
+
+
+.intro,
+.text-intro,
+.charts,
+.cards {
     --color-base: #1E1E1E;
     color: #fff;
-    --background-container: linear-gradient(
-            to bottom, 
-            var(--color-base), 
-            color-mix(in srgb, var(--color-base) 95%, white)
-        );
+    --background-container: linear-gradient(to bottom,
+            var(--color-base),
+            color-mix(in srgb, var(--color-base) 95%, white));
+    background: var(--background-container);
+    padding-left: 5vw;
+    padding-right: 5vw;
+    display: flex;
 }
 
-.intro, 
-.text-intro, 
-.charts, 
-.cards{
-        background:var(--background-container);
-        padding-left: 5vw;
-        padding-right: 5vw;
-        display: flex;
-}
-
-.intro{
+.intro {
     padding-top: 4%;
     flex-direction: column;
-    text-align: center;   
-    gap: 15%; 
+    text-align: center;
+    gap: 15%;
+
     .intro-title {
-        
+
         margin-bottom: 15px;
         padding-bottom: 15px;
         border-bottom: 2px solid #ccc;
     }
 }
 
-.text-intro{
+.text-intro {
     flex-direction: column;
     padding: 5vw;
     align-items: center;
     gap: 15%;
 }
-.cards{
+
+.cards {
     flex-direction: column;
-    .educativo, 
+
+    .educativo,
     .interactivo,
-    .medicion-real{
-        padding: 2%;
+    .medicion-real {
         background-color: #272727;
         border: #484848 solid 1px;
-        border-radius: 5px;
+        border-radius: 15px;
     }
 
-    p{
+    p {
         border-left: #FFE77A solid 1px;
     }
 }
-.font-black{
+
+.font-black {
     color: black !important;
 }
-.btn-rendimiento{
+
+.btn-rendimiento {
     background-color: #FFE77A;
     border-radius: 5px;
-    
+
 }
 
-.charts{
+.charts {
     display: flex;
     justify-content: center;
-    #grafica-barra{
+
+    #grafica-barra {
         height: 400px !important;
     }
 }
